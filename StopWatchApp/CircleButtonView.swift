2d3cad766a2e88072e262c84dc7d0e7ad6147ff3
Nina Zhou <nzhou@lcs.on.ca>
2023-10-30 12:00:09 -0400

    created CircleButtonView as a helper to make ContentView shorter.

--- StopWatchApp/CircleButtonView.swift
+++ StopWatchApp/CircleButtonView.swift
@@ -9,7 +9,23 @@ import SwiftUI
 
 struct CircleButtonView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            //first layer
+            Circle()
+                .foregroundColor(.gray)
+                .frame(width: 100)
+            //second layer
+            Circle()
+                .foregroundStyle(.black)
+                .frame(width: 93)
+            //third layer
+            Circle()
+                .foregroundStyle(.gray)
+                .frame(width: 89)
+            Text("Reset")
+                .foregroundStyle(.white)
+                .font(.title2)
+        }
     }
 }
 
