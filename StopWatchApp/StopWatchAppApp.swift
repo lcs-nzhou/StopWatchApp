5474683d784d728dc80b8cab7969ea085c28896b
Nina Zhou <nzhou@lcs.on.ca>
2023-10-30 11:32:53 -0400

    Initial Commit

---
+++ StopWatchApp/StopWatchAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  StopWatchAppApp.swift
+//  StopWatchApp
+//
+//  Created by Shuyu Zhou on 2023-10-30.
+//
+
+import SwiftUI
+
+@main
+struct StopWatchAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
